3e74b1057d2d1db8626506d03845e63fd6f2e392
JioBani <spdlqj8876@gmail.com>
2025-10-10 00:58:39 +0900

    TACD-30 유닛 데이터 SO 제작

---
+++ Assets/Scenes/Battle/Scripts/Unit/ISkillCaster.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Scenes.Battle.Scripts.Unit
+{
+    public interface ISkillCaster
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Battle/Scripts/Unit/Skills/SkillEnum.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Scenes.Battle.Scripts.Unit.Skills
+{
+    public enum SkillEnum
+    {
+        TestSkill,
+    }
+}
